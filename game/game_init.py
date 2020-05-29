85f2f4132371e8e0e669bedd3f0681ec494b67a3
Connor McGuile <cmcguile@gmail.com>
2020-05-29 17:48:48 +0100

    Tile select highlighting added

--- game/game_init.py
+++ game/game_init.py
@@ -24,6 +24,9 @@ drag_surf = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA, 32).convert_alpha()
 drag_surf_rect = drag_surf.get_rect()
 player_move = 'white'
 
+hexa_selected = None
+offset_x, offset_y = 0, 0
+
 board = Board(40, 40)
 
 top_rack = Board(3, 5)
@@ -63,43 +66,35 @@ board.board[7][7] = Ant(W)
 
 
 def setup_racks(rack, top=True):
-    y = 0  # if start == 0 else RACK_HEIGHT - 60
+    y = 0
+    rack_surf = rack_top_surf if top else rack_bottom_surf
+    rack_surf.fill((0, 0, 0, 0))
     for r, _ in enumerate(rack.board):
         x = 120
-        y += 20  # if start == 0 else -20
-        for _, hexa in enumerate(rack.board[r]):
-            rack_surf = rack_top_surf if top else rack_bottom_surf
+        y += 20
+        for hexa in rack.board[r]:
             rack_surf.blit(hexa.image, (x, y))
+            size = hexa.image.get_rect().size
+            hexa.rect = pygame.Rect((x, y), size)
             x += 150
 
 
 def draw_placed_tiles():
     x_o, y_o = 0, 0
+    drag_surf.fill((0, 0, 0, 0))
     for r, _ in enumerate(board.board):
         x, y = x_o, y_o
         for c, hexa in enumerate(board.board[r]):
             drag_surf.blit(hexa.image, (x, y))
+            size = hexa.image.get_rect().size
+            hexa.rect = pygame.Rect((x, y), size)
             y += 30 if c % 2 == 0 else -30
             x += 52
         y_o += 61
 
 
-# def draw_possible_moves():
-#     x_o, y_o = 0, 0
-#     for r, _ in enumerate(board.board[3:-3]):
-#         x, y = x_o, y_o
-#         for c, hexa in enumerate(board.board[r + 3]):
-#
-#             drag_surf.blit(hexa.image, (x, y))
-#             y += 30 if c % 2 == 0 else -30
-#             x += 52
-#         y_o += 61
-
-
 def get_possible_moves_from_rack(player):
-    global debug
     possible_moves = []
-    f_x, f_y, v_x, v_y = None, None, None, None
     for r, _ in enumerate(board.board):
         for c, hexa in enumerate(board.board[r]):
             neighbours = get_cell_neighbours(r, c)
@@ -108,15 +103,15 @@ def get_possible_moves_from_rack(player):
             for x, y in neighbours:
                 if board.board[x][y].player == player:
                     found_own_tile = True
-                    f_x, f_y = x, y
-                elif board.board[x][y].player == opp(player):
+                    # print((r,c), f'found {player} at', (x,y))
+                elif board.board[x][y].player == opp(player) or board.board[r][c].player == opp(player):
+                    # print((r,c),f'found {opp(player)} at', (x,y))
                     valid_placement = False
-                    v_x, v_y = x, y
-            # if debug and found_own_tile:
-            #     print((r, c), (f_x, f_y), (v_x, v_y))
             if found_own_tile and valid_placement:
                 board.board[r][c].image = pygame.image.load(f'../img_assets/possible.png')
-    debug = False
+                possible_moves.append((r, c))
+                # print(f'placing possible at {(r,c)}')
+    return possible_moves
 
 
 def get_cell_neighbours(r, c):
@@ -154,36 +149,66 @@ def draw_board():
     screen.blit(rack_top_surf, (0, 0))
     screen.blit(rack_bottom_surf, (0, HEIGHT - RACK_HEIGHT))
     setup_racks(top_rack)
-    setup_racks(bottom_rack, False)
+    setup_racks(bottom_rack, top=False)
     get_possible_moves_from_rack(W)
     draw_placed_tiles()
 
 
-def get_human_move():
-    dragging = False
-    offset_x, offset_y = 0, 0
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and not dragging and drag_surf_rect.collidepoint(event.pos):
-                dragging = True
-                x, y = event.pos
-                offset_x, offset_y = drag_surf_rect.centerx - x, drag_surf_rect.centery - y
-            if event.type == pygame.MOUSEMOTION and dragging:
-                mouse_x, mouse_y = event.pos
-                drag_surf_rect.centerx = mouse_x + offset_x
-                drag_surf_rect.centery = mouse_y + offset_y
-            elif event.type == pygame.MOUSEBUTTONUP and dragging:
-                dragging = False
+def event_handler_select_tile(event, dragging):
+    global hexa_selected
+    img_selected = pygame.image.load('../img_assets/selected.png')
+    if event.type == pygame.MOUSEBUTTONDOWN and not dragging:
+        _, y = event.pos
+        if y < RACK_HEIGHT:
+            for row in range(top_rack.width):
+                for hexa in top_rack.board[row]:
+                    if hexa.rect.collidepoint(event.pos) and type(hexa) != Blank:
+                        if hexa_selected:
+                            hexa_selected.image = pygame.image.load(hexa_selected.image_loc)
+                        hexa.image.blit(img_selected, (0, 0))
+                        hexa_selected = hexa
+        elif y > HEIGHT - RACK_HEIGHT:
+            for row in range(bottom_rack.width):
+                for hexa in bottom_rack.board[row]:
+                    x, y = event.pos
+                    y = y - HEIGHT + RACK_HEIGHT
+                    if hexa.rect.collidepoint((x, y)) and type(hexa) != Blank:
+                        if hexa_selected:
+                            hexa_selected.image = pygame.image.load(hexa_selected.image_loc)
+                        hexa.image.blit(img_selected, (0, 0))
+                        hexa_selected = hexa
+        else:
+            for row in range(board.width):
+                for hexa in board.board[row]:
+                    if hexa.rect.collidepoint(event.pos) and type(hexa) != Blank:
+                        if hexa_selected:
+                            hexa_selected.image = pygame.image.load(hexa_selected.image_loc)
+                        hexa.image.blit(img_selected, (0, 0))
+                        hexa_selected = hexa
 
-        draw_board()
-        pygame.display.update()
-        clock.tick(30)
+
+def event_handler_board_drag(event, dragging):
+    global offset_x
+    global offset_y
+    event_handler_select_tile(event, dragging)
+
+    if event.type == pygame.MOUSEBUTTONDOWN and not dragging and drag_surf_rect.collidepoint(event.pos):
+        dragging = True
+        mouse_x, mouse_y = event.pos
+        offset_x = drag_surf_rect.x - mouse_x
+        offset_y = drag_surf_rect.y - mouse_y
+    elif event.type == pygame.MOUSEMOTION and dragging:
+        mouse_x, mouse_y = event.pos
+        drag_surf_rect.x = mouse_x + offset_x
+        drag_surf_rect.y = mouse_y + offset_y
+    elif event.type == pygame.MOUSEBUTTONUP and dragging:
+        offset_x, offset_y = 0, 0
+        dragging = False
+    return dragging
 
 
 def run_game():
+    dragging = False
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -191,8 +216,13 @@ def run_game():
                 sys.exit()
 
             else:
-                get_human_move()
+                #dragging = event_handler_board_drag(event, dragging)
+                event_handler_select_tile(event, dragging)
+
+        draw_board()
+        pygame.display.update()
+        clock.tick(30)
 
 
 run_game()
-# print(get_cell_neighbours(5,6))
+# print(get_possible_moves_from_rack(B))
--- game/insects.py
+++ game/insects.py
@@ -6,52 +6,103 @@ class Bee:
         self.image = pygame.image.load(f'../img_assets/{player}_bee.png')
         self.in_play = False
         self.player = player
+        self.image_loc = f'../img_assets/{self.player}_bee.png'
+        self.rect = None
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Bee):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.player == other.player and self.rect == other.rect
 
 
 class Ant:
     def __init__(self, player, x=0, y=0):
         self.image = pygame.image.load(f'../img_assets/{player}_ant.png')
+        self.image_o = pygame.image.load(f'../img_assets/{player}_ant.png')
         self.in_play = False
         self.player = player
+        self.image_loc = f'../img_assets/{self.player}_ant.png'
+        self.rect = None
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Ant):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.player == other.player and self.rect == other.rect
 
 
 class Beetle:
     def __init__(self, player, x=0, y=0):
         self.image = pygame.image.load(f'../img_assets/{player}_beetle.png')
+        self.image_o = pygame.image.load(f'../img_assets/{player}_beetle.png')
         self.in_play = False
         self.player = player
+        self.image_loc = f'../img_assets/{self.player}_beetle.png'
+        self.rect = None
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Beetle):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.player == other.player and self.rect == other.rect
 
 
 class Grasshopper:
     def __init__(self, player, x=0, y=0):
         self.image = pygame.image.load(f'../img_assets/{player}_grasshopper.png')
+        self.image_o = pygame.image.load(f'../img_assets/{player}_grasshopper.png')
         self.in_play = False
         self.player = player
+        self.image_loc = f'../img_assets/{self.player}_grasshopper.png'
+        self.rect = None
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Grasshopper):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.player == other.player and self.rect == other.rect
 
 
 class Spider:
     def __init__(self, player, x=0, y=0):
         self.image = pygame.image.load(f'../img_assets/{player}_spider.png')
+        self.image_o = pygame.image.load(f'../img_assets/{player}_spider.png')
         self.in_play = False
         self.player = player
+        self.image_loc = f'../img_assets/{self.player}_spider.png'
+        self.rect = None
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Spider):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.player == other.player and self.rect == other.rect
 
 
 class Blank:
     def __init__(self, x=0, y=0):
         self.image = pygame.image.load(f'../img_assets/blank.png')
+        self.image_o = pygame.image.load(f'../img_assets/blank.png')
         self.in_play = False
         self.playable = True
         self.player = None
+        self.rect = None
         self.x = x
         self.y = y
 
@@ -59,6 +110,8 @@ class Blank:
 class Possible:
     def __init__(self, x=0, y=0):
         self.image = pygame.image.load(f'../img_assets/possible.png')
+        self.image_o = pygame.image.load(f'../img_assets/possible.png')
         self.in_play = False
+        self.rect = None
         self.x = x
         self.y = y
