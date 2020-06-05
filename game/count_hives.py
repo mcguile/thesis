c5aa130d4e7367a12dc1834cb43fe2a2a13eaced
mcguile <cmcguile@gmail.com>
2020-06-05 14:06:15 +0100

    comments

--- game/count_hives.py
+++ game/count_hives.py
@@ -2,29 +2,26 @@ from insects import Blank
 
 
 class HiveGraph:
-
     def __init__(self, board):
         self.ROW = board.height
         self.COL = board.width
         self.graph = board.board
 
-    # A function to check if a given cell
-    # (row, col) can be included in DFS
+    # A function to check if a given hexagon can be included in DFS
     def is_safe(self, row, col, visited):
-        # row number is in range, column number
-        # is in range and value is 1
-        # and not yet visited
+        # row number is in range,
+        # column number is in range,
+        # and hexagon is Blank and not yet visited
         return (0 <= row < self.ROW and
                 0 <= col < self.COL and
                 not visited[row][col] and type(self.graph[row][col]) != Blank)
 
-    # A utility function to do DFS for a 2D
-    # boolean matrix. It only considers
-    # the 6 neighbours as adjacent vertices
+    # DFS for a 2D matrix. It only considers
+    # the 6 neighbours as adjacent pieces
     def dfs(self, row, col, visited):
         # These arrays are used to get row and
-        # column numbers of 8 neighbours
-        # of a given cell
+        # column numbers of 6 neighbours
+        # of a given hexagon
         if col % 2 == 1:
             row_nbr = [-1,  0, 0,  1, 1, 1]
             col_nbr = [ 0, -1, 1, -1, 0, 1]
@@ -32,7 +29,7 @@ class HiveGraph:
             row_nbr = [-1, -1, -1,  0, 0, 1]
             col_nbr = [-1,  0,  1, -1, 1, 0]
 
-        # Mark this cell as visited
+        # Mark this hexagon as visited
         visited[row][col] = True
 
         # Recur for all connected neighbours
@@ -41,22 +38,20 @@ class HiveGraph:
                 self.dfs(row + row_nbr[k], col + col_nbr[k], visited)
 
     def count_hives(self):
-        # Make a bool array to mark visited cells.
-        # Initially all cells are unvisited
+        # Make a bool array to mark visited hexagons.
+        # Initially all hexagons are unvisited
         visited = [[False for _ in range(self.COL)] for _ in range(self.ROW)]
 
-        # Initialize count as 0 and travese
-        # through the all cells of
-        # given matrix
+        # Initialize count as 0 and traverse
+        # through the all hexagons of given matrix
         count = 0
         for row in range(self.ROW):
             for col in range(self.COL):
-                # If a cell with value 1 is not visited yet,
+                # If a hexagon not Blank and is not visited yet,
                 # then new island found
                 if not visited[row][col] and type(self.graph[row][col]) != Blank:
-                    # Visit all cells in this island
-                    # and increment island count
+                    # Visit all hexagons in this hive
+                    # and increment hive count
                     self.dfs(row, col, visited)
                     count += 1
-
         return count
